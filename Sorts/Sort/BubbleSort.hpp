003e160700c61a610efc49f21a0c18400ef2f947
ycshao21 <mematthew21@gmail.com>
2023-06-13 00:33:50 +0800

    bubble sort - last swap

--- Sorts/Sort/BubbleSort.hpp
+++ Sorts/Sort/BubbleSort.hpp
@@ -3,10 +3,10 @@
 
 #include <algorithm>
 
-namespace mysort {
+namespace mysort
+{
     template<class _RandIt, class _Pr = std::less<void>>
-    void BubbleSort(_RandIt seq, indext first, indext last, const _Pr& comp = {})
-    {
+    void BubbleSort(_RandIt seq, indext first, indext last, const _Pr& comp = {}) {
         sizet size = last - first + 1;
         for (indext i = 1; i < size; ++i) {
             for (indext j = 0; j < size - i; ++j) {
@@ -18,8 +18,7 @@ namespace mysort {
     }
 
     template<class _RandIt, class _Pr = std::less<void>>
-    void BubbleSort_Stop(_RandIt seq, indext first, indext last, const _Pr& comp = {})
-    {
+    void BubbleSort_Stop(_RandIt seq, indext first, indext last, const _Pr& comp = {}) {
         sizet size = last - first + 1;
         bool swapped = false;
         for (indext i = 1; i < size; ++i) {
@@ -31,6 +30,19 @@ namespace mysort {
             }
             if (swapped == false) return;
             swapped = false;
+        }
+    }
+
+    template<class _RandIt, class _Pr = std::less<void>>
+    void BubbleSort_LastSwap(_RandIt seq, indext first, indext last, const _Pr& comp = {}) {
+        indext lastSwap = 0;
+        for (sizet boundary = last - first; boundary > 0; boundary = lastSwap, lastSwap = 0) {
+            for (indext j = 0; j < boundary; ++j) {
+                if (comp(*(seq + j + 1), *(seq + j))) {
+                    std::iter_swap(seq + j, seq + j + 1);
+                    lastSwap = j;
+                }
+            }
         }
     }
 }
